f5151837f8daf019b77f5ef8c3c77bb57503ac9c
jrazi <j.razi@outlook.com>
2019-10-19 20:23:58 +0330

    Get course info

--- enroll.py
+++ enroll.py
@@ -7,6 +7,8 @@ import json
 messages = {
     "INVALID_COMMAND": "Please enter the command in below format:\npython3 enroll.py -u [username] -p [password]"
 }
+
+course_info = {}
 driver = None
 config= {}
 
@@ -44,8 +46,25 @@ def login():
     password_field.send_keys(config['user_info']['password'])
     password_field.send_keys(Keys.ENTER)
 
+def get_semester_no(year, season):
+    year = str(year)
+    return (year + '1') if (season == 'fall' or season == 'winter') else str(int(year) - 1) + '2' if season == 'spring' else str(int(year) - 1) + '3'
+
+def get_course_info():
+    global course_info
+
+    course_name= course_info["course_name"] = driver.find_element_by_xpath('//h3[@class="coursename"]/a').text
+    course_name.split()
+
+    course_info["course_words"] = [word for word in course_name.split() if word.isalpha()]
+    course_info["first_letters"] = [word[0] for word in course_info["course_words"]]
+
+    course_info["season"], course_info["year"]= driver.title.split()[-2].lower(), driver.title.split()[-1]
+    course_info["semester"] = get_semester_no(course_info["year"], course_info["season"])
+
 def enroll(course_url):
     driver.get(course_url)
+    get_course_info()
 
 def main():
     global config
